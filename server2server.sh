aac8b08c7dd71300225617d7ada9d8bfbedce0be
Fernando García Hansen <fgarcia@humanoide.es>
2019-06-03 23:32:05 +0200

    Create server2server.sh

---
+++ server2server.sh
@@ -0,0 +1,1 @@
+# tar cvpf - --exclude=/etc/network/* --exclude=/etc/hosts --exclude=/proc/* --exclude=/dev/* --exclude=/lost+found --exclude=/etc/fstab --exclude=/etc/mtab --exclude=/sys/* --exclude=/boot/* / | ssh root@ip.de.destino "tar xvpf - -C /"
