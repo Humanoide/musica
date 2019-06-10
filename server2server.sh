d9dfe3ccba5c5838f30d356a32df49b5b16bccbf
Fernando García Hansen <fgarcia@humanoide.es>
2019-06-10 11:21:03 +0200

    Update server2server.sh

--- server2server.sh
+++ server2server.sh
@@ -1,1 +1,1 @@
-# tar cvpf - --exclude=/etc/network/* --exclude=/etc/hosts --exclude=/proc/* --exclude=/dev/* --exclude=/lost+found --exclude=/etc/fstab --exclude=/etc/mtab --exclude=/sys/* --exclude=/boot/* / | ssh root@ip.de.destino "tar xvpf - -C /"
+# tar cvpf - --exclude=/etc/network/* --exclude=/etc/network/interfaces --exclude=/etc/resolv.conf --exclude=/etc/hosts --exclude=/proc/* --exclude=/dev/* --exclude=/lost+found --exclude=/etc/fstab --exclude=/etc/mtab --exclude=/sys/* --exclude=/boot/* / | ssh root@ip.de.destino "tar xvpf - -C /"
